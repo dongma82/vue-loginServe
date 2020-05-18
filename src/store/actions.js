293983d2ce88da9c8e881c5ff6d12853ef848cb3
cheng <1363830397@qq.com>
2020-05-18 20:16:28 +0800

    first

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,81 +1,77 @@
 import $axios from 'axios';
 
-$axios.defaults.baseURL = '/api';
+$axios.defaults.baseURL = 'https://api.mengdigua.cn';
 let actions = {
     // 主营类目
     mainCate(store, data) {
         return $axios.post('/shop/shop-apply-record-api/business-category-list', data);
     },
     xieyi(store, data) {
-        return $axios.post('/content/content-api/view', data);//协议
+        return $axios.post('/content/content-api/view', data); //协议
     },
     pingtaiMsg(store, data) {
-        return $axios.post('/base/intel-api/info', data);//平台信息
+        return $axios.post('/base/intel-api/info', data); //平台信息
     },
     zuce(store, data) {
-        return $axios.post('/member/user-api/web-register', data);//注册
+        return $axios.post('/member/user-api/web-register', data); //注册
     },
     forCode(store, data) {
-        return $axios.post('/member/user-api/web-send-sms', data);// 获取验证码 登录 和注册
+        return $axios.post('/member/user-api/web-send-sms', data); // 获取验证码 登录 和注册
     },
     login(store, data) {
-        return $axios.post('/member/user-api/web-login', data);// // 登录
+        return $axios.post('/member/user-api/web-login', data); // // 登录
     },
     storeApply(store, data) {
-        return $axios.post('/shop/shop-apply-record-api/create', data);//店铺申请入驻
+        return $axios.post('/shop/shop-apply-record-api/create', data); //店铺申请入驻
     },
     queryImgCode(store, data) {
         console.log(store)
-
-        return $axios.post('/member/user-api/get-img-code', data);//图片二维码
+        return $axios.post('/member/user-api/get-img-code', data); //图片二维码
     },
     shopDetail(store, data) {
-       return $axios.post('/shop/shop-apply-record-api/detail', {
-            token: store.state.token
-        }).then(res => {
-    
-            let d = res.data;
-            if (d.errcode == 0) {
-                store.commit("changeMyShop",d.data);
-                if (d.data) {
-                    if (d.data.status) {
-                        store.commit("changeResult",d.data.status);
-                        store.commit("changeReason",d.data.reason);
-                        store.commit("changeStep",4) ;
-                        store.commit("changeIsSet",true)
+        return $axios.post('/shop/shop-apply-record-api/detail', {
+                token: store.state.token
+            }).then(res => {
+
+                let d = res.data;
+                if (d.errcode == 0) {
+                    store.commit("changeMyShop", d.data);
+                    if (d.data) {
+                        if (d.data.status) {
+                            store.commit("changeResult", d.data.status);
+                            store.commit("changeReason", d.data.reason);
+                            store.commit("changeStep", 4);
+                            store.commit("changeIsSet", true)
+                        }
+                    } else {
+                        store.commit("changeStep", 1);
+                        store.commit("changeIsSet", false)
                     }
                 } else {
-                    store.commit("changeStep",1) ;
-                    store.commit("changeIsSet",false)
-                }
-            } else {
-                store.commit("changeStep",1) ;
-                store.commit("changeIsSet",false)
-            };
-        })
-        .catch(d => {
-         
-            store.commit("changeStep",1) ;
-            
-        });;//获取店铺信息
+                    store.commit("changeStep", 1);
+                    store.commit("changeIsSet", false)
+                };
+            })
+            .catch(d => {
+
+                store.commit("changeStep", 1);
+
+            });; //获取店铺信息
     },
     storeApply(store, data) {
-        return $axios.post('/shop/shop-apply-record-api/create', data);//店铺申请入驻
+        return $axios.post('/shop/shop-apply-record-api/create', data); //店铺申请入驻
     },
     changeDetail(store, data) {
-        return $axios.post('/shop/shop-apply-record-api/update', data);//店铺信息修改
+        return $axios.post('/shop/shop-apply-record-api/update', data); //店铺信息修改
     },
-    reset(store,data){
-        return $axios.post('/member/user-api/web-reset-password', data);//密码重置
+    reset(store, data) {
+        return $axios.post('/member/user-api/web-reset-password', data); //密码重置
     },
     resetCode(store, data) {
-        return $axios.post('/member/user-api/send-sms', data);// 获取验证码修改密码
+        return $axios.post('/member/user-api/send-sms', data); // 获取验证码修改密码
     },
     // downLoadWord(){
     //     return $axios.post('/member/user-api/web-reset-password', data);//密码重置
     // }
 }
 export default actions
-
-
-
--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,19 @@
+// module.exports = {
+//     devServer: {
+//         proxy: {
+//             '/api': { //这里最好有一个 /
+//                 target: 'https://api-test.mengdigua.cn', // 后台接口域名
+//                 // ws: true,        //如果要代理 websockets，配置这个参数
+//                 secure: false, // 如果是https接口，需要配置这个参数
+//                 changeOrigin: false, //是否跨域
+//                 pathRewrite: {
+//                     '^/api': "/"
+//                 },
+//                 headers: {
+//                     Referer: 'https://api-test.mengdigua.cn'
+//                 }
+//             },
 
-module.exports = {
-    devServer: {
-        proxy: {
-            '/api': {     //这里最好有一个 /
-                target: 'https://api-test.mengdigua.cn',  // 后台接口域名
-                // ws: true,        //如果要代理 websockets，配置这个参数
-                secure: false,  // 如果是https接口，需要配置这个参数
-                changeOrigin:false,  //是否跨域
-                pathRewrite: {
-                    '^/api': ''
-                },
-                headers: {
-                    Referer: 'https://api-test.mengdigua.cn'
-                }
-            },
-           
-        }
-    }
-}
+//         }
+//     }
+// }
